fe58719198c434f35fb60afe48356e0de9052eee
Gabrielvis1 <gabrielvis1214ar@gmail.com>
2025-04-19 09:54:15 -0300

    empezamos login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms'
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {
+
+  fb = inject(FormBuilder)
+  hasError = signal(false)
+  isPosting = signal(false)
+
+  loginForm = this.fb.group({
+
+  })
+
+}
